344b5f1559ca68c764cc3a349ad1ef668c23a7f1
nrangasa <nandakumar.rangasamy@oracle.com>
2022-11-10 02:01:08 -0800

    updated

---
+++ financy/src/index.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+
+
+let b_calc = document.getElementById('b_calc')
+
+let simpleInterest = (principal,years,rate) => {
+    let per_month = ((principal * rate) / 100 ) / 12
+    let marturity_amount =  Math.round((per_month * 12 * years));
+
+    return {
+      p_month: per_month,
+      m_amount: marturity_amount,
+    };
+}
+
+b_calc.addEventListener('click', () => {
+
+    let p_amount = parseFloat(document.getElementById("p_amount").value);
+    let i_rate = parseFloat(document.getElementById("i_rate").value);
+    let years = parseFloat(document.getElementById("years").value);
+
+    let result = simpleInterest(p_amount, years, i_rate);
+    
+
+    document.getElementById('p_month').textContent = result.p_month
+    document.getElementById("m_amount").textContent = result.m_amount;
+    let url = "http://127.0.0.1:5000/interest";
+    axios.post(url, {
+      p_amount
+    }).then(data => {
+      console.log(data);
+    })
+})
+
